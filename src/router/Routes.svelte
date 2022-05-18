5fcf4bd97bfcdf76dca6d74441e00909cdfc367d
James Logan <james@dbej.net>
2022-05-18 16:53:18 -0400

    created two test pages

--- src/router/Routes.svelte
+++ src/router/Routes.svelte
@@ -1,8 +1,17 @@
 <script lang='ts'>
     import { Router, Route, Link } from "svelte-navigator";
+import Homepage from "../pages/Homepage.svelte";
     import Notfound from "../pages/Notfound.svelte";
     import PrivateRoute from "./PrivateRoute.svelte";
 </script>
+
+<Route path="./">
+    <Homepage />
+</Route>
+
+<Route path="./2">
+    <Homepage />
+</Route>
 
 <Route>
     <Notfound />
